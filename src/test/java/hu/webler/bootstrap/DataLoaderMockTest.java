c33da8f0b4d70bc754ec407f38ae86fd00df9796
kmatthew1992 <koszegim8@gmail.com>
2024-03-27 11:22:42 +0100

    mockito test

---
+++ src/test/java/hu/webler/bootstrap/DataLoaderMockTest.java
@@ -0,0 +1,14 @@
+package hu.webler.bootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class DataLoaderMockTest {
+
+    @Test
+    public void testLoadData() {
+
+        // Create mock objects
+        DataLoader mockDataLoader = Mockito.mock();
+    }
+}
